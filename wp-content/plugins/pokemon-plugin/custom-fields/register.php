dea574cc9ad4aead670f0e2f67149af163f58dcf
Huras Alexandre <hurast@gmail.com>
2022-07-19 16:39:55 -0300

    event more custom fields on the pokemon rest api resource

--- wp-content/plugins/pokemon-plugin/custom-fields/register.php
+++ wp-content/plugins/pokemon-plugin/custom-fields/register.php
@@ -3,9 +3,19 @@
 function Pokeplugin_add_new_fields() {
     register_rest_field( 
         'pokemon',     // Object(s) the filed is being registered to
-        'xaxax', // Attribute (field) name
+        'html_pkmn_types', // Attribute (field) name
          array(     // Array of arguments
             'get_callback'    => 'Pokeplugin_get_category_links', // Retrieves the field value.
+            'update_callback' => null,                 // Updates the field value.
+            'schema'          => null,                 // Creates schema for the field.
+        ) 
+    );
+
+    register_rest_field( 
+        'pokemon',     // Object(s) the filed is being registered to
+        'previous_post', // Attribute (field) name
+         array(     // Array of arguments
+            'get_callback'    => 'Pokeplugin_get_previous_post', // Retrieves the field value.
             'update_callback' => null,                 // Updates the field value.
             'schema'          => null,                 // Creates schema for the field.
         ) 
@@ -13,14 +23,23 @@ function Pokeplugin_add_new_fields() {
 }
 
 function Pokeplugin_get_category_links() {
-    $categories = get_the_category();
+    $terms = get_the_terms(get_post(), 'pokemon_type');
     $separator = ', ';
     $output = '';
-    if ( ! empty( $categories ) ) {
-        foreach( $categories as $category ) {
-            $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
+    if ( ! empty( $terms ) ) {
+        foreach( $terms as $term ) {
+            $output .= '<a href="'.get_term_link($term, 'pokemon_type').'">' . esc_html( $term->name ) . '</a>' . $separator;
         }
         $output = trim( $output, $separator );
     }
     return $output;
+}
+
+function Pokeplugin_get_previous_post() {
+    $previous_post = get_previous_post();
+    return [
+        'id' => $previous_post->ID,
+        'title' => $previous_post->post_title,
+        'link' => get_permalink($previous_post)
+    ];
 }
